8ab4de4c12eebc2b07c2cacb3f9515397207610d
Diana Clemente <dianaeclemente@gmail.com>
2022-11-06 15:14:26 -0500

    test nov 6 save

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { getUserById } = require("./db");
+const { JWT_SECRET } = process.env;
+const app = express();
 
 // Setup your Middleware and API Router here
 
+app.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
+
+  if (!auth) {
+    // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
+app.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
+
+const router = require("./api");
+app.use("/api", router);
 module.exports = app;
